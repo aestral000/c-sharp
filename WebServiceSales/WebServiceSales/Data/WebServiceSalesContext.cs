eda293f525628b16dece2db6dd8de697c992e83d
Alan Nunes <alan.nunes@triplos.com.br>
2021-05-21 10:34:49 -0300

    Migration com o scaffolding realizado e conectado com a base de dados da triplos

---
+++ WebServiceSales/WebServiceSales/Data/WebServiceSalesContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebServiceSales.Models.EntityModels;
+
+namespace WebServiceSales.Data
+{
+    public class WebServiceSalesContext : DbContext
+    {
+        public WebServiceSalesContext (DbContextOptions<WebServiceSalesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebServiceSales.Models.EntityModels.Department> Department { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            optionsBuilder.UseSqlServer("Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=WebSales;Data Source=NOTE0050");
+        }
+    }
+}
